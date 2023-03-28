46b1ce1a69e9d5ddf60536426302d5cd1303b417
tiennd12 <tien1241999@gmail.com>
2023-03-28 11:31:43 +0700

    git: update

--- client/src/features/users/UsersList.js
+++ client/src/features/users/UsersList.js
@@ -1,53 +1,51 @@
-import { useGetUsersQuery } from "./usersApiSlice";
-import User from "./User";
+import { useGetUsersQuery } from "./usersApiSlice"
+import User from './User'
+import PulseLoader from 'react-spinners/PulseLoader'
 
 const UsersList = () => {
-  const {
-    data: users,
-    isLoading,
-    isSuccess,
-    isError,
-    error,
-  } = useGetUsersQuery("UsersList", {
-    pollingInterval: 60000,
-    refetchOnFocus: true,
-    refetchOnMountOrArgChange: true,
-  });
 
-  let content;
+    const {
+        data: users,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetUsersQuery('usersList', {
+        pollingInterval: 60000,
+        refetchOnFocus: true,
+        refetchOnMountOrArgChange: true
+    })
 
-  if (isLoading) content = <p>Loading...</p>;
+    let content
 
-  if (isError) {
-    content = <p className="errmsg">{error?.data?.message}</p>;
-  }
+    if (isLoading) content = <PulseLoader color={"#FFF"} />
 
-  if (isSuccess) {
-    const { ids } = users;
+    if (isError) {
+        content = <p className="errmsg">{error?.data?.message}</p>
+    }
 
-    const tableContent =
-      ids?.length && ids.map((userId) => <User key={userId} userId={userId} />);
+    if (isSuccess) {
 
-    content = (
-      <table className="table table--users">
-        <thead className="table__thead">
-          <tr>
-            <th scope="col" className="table__th user__username">
-              Username
-            </th>
-            <th scope="col" className="table__th user__roles">
-              Roles
-            </th>
-            <th scope="col" className="table__th user__edit">
-              Edit
-            </th>
-          </tr>
-        </thead>
-        <tbody>{tableContent}</tbody>
-      </table>
-    );
-  }
+        const { ids } = users
 
-  return content;
-};
-export default UsersList;
+        const tableContent = ids?.length && ids.map(userId => <User key={userId} userId={userId} />)
+
+        content = (
+            <table className="table table--users">
+                <thead className="table__thead">
+                    <tr>
+                        <th scope="col" className="table__th user__username">Username</th>
+                        <th scope="col" className="table__th user__roles">Roles</th>
+                        <th scope="col" className="table__th user__edit">Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableContent}
+                </tbody>
+            </table>
+        )
+    }
+
+    return content
+}
+export default UsersList
